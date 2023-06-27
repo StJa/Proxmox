750eedac96feab7d08a318902d8101126ca1f310
Stephan Jansen <stephan.jansen@ise.de>
2023-06-27 13:04:54 +0200

    install a plain nextcloud lxc

---
+++ install/nextcloud-install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2023 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
+msg_ok "Installed Dependencies"
+
+
+read -r -p "What is your domain for nextcloud: " NEXTCLOUDDNS
+read -r -p "Do you want Nextcloud office to be installed: " NEXTCLOUDOFFICE
+
+msg_info "Installing NextCloud (Patience)"
+$STD bash <(curl -fsSL https://codeberg.org/criegerde/nextcloud-zero/raw/branch/master/debian.sh | sed "s/NEXTCLOUDDNS=\"ihre.domain.de\"/NEXTCLOUDDNS=\"${NEXTCLOUDDNS}\"/g" | sed "s/NEXTCLNEXTCLOUDOFFICE=\"n\"/NEXTCLOUDOFFICE=\"${nextcloudoffice}\"/g")
+msg_ok "Installed NextCloud"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get autoremove
+$STD apt-get autoclean
+msg_ok "Cleaned"
